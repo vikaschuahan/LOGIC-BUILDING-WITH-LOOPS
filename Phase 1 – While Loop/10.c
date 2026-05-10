de35a6c4ca8c570bc635786d7c893a7ffa7f80e4
vikaschuahan <vikasparvinbhai@gmail.com>
2026-05-11 00:19:53 +0530

    9 and 10 thoduk tuff hatu product numbr print and sum karvo pan ty gyu

--- Phase 1 – While Loop/10.c
+++ Phase 1 – While Loop/10.c
@@ -1,1 +1,21 @@
 // 10. Find and print the product of all digits of a given number.
+#include <stdio.h>
+
+int main()
+{
+
+    int n, r, sum = 0;
+    printf("Enter Number:");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        r = n % 10;
+
+        sum = sum + r;
+
+        n = n / 10;
+    }
+    printf("\n%d", sum);
+    return 0;
+}
--- Phase 1 – While Loop/9.c
+++ Phase 1 – While Loop/9.c
@@ -1,1 +1,19 @@
 // 9. Calculate and print the factorial of a given number.
+#include <stdio.h>
+
+int main()
+{
+    int i = 1, j, fact = 1;
+
+    printf("Enter NUmber: ");
+    scanf("%d", &j);
+
+    while (i <= j)
+    {
+        fact = fact * i;
+        i++;
+    }
+
+    printf("\n%d", fact);
+    return 0;
+}
